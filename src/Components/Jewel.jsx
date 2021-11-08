6fa98d7616403429f1f4d9278494ce7306e6babd
Egidijus <egidijusx@gmail.com>
2021-11-08 12:07:27 +0200

    some css

--- src/Components/Jewel.jsx
+++ src/Components/Jewel.jsx
@@ -2,8 +2,16 @@ function Jewel ({bling}) {
     
     return (
         <div className="jewelry__list__bling">
-            <div>{bling.product}</div>
-            
+            <div className="jewelry__list__bling__item">
+                <span>{bling.product}</span>
+                <span>{bling.quantity}</span>
+                <span>{bling.price}</span>
+                <span>yes or no</span>
+                <span>{bling.in_stock}</span>
+                <span>{bling.last_order.slice(0,10)}</span>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
         </div>
     )
 }
--- src/Components/JewelryList.jsx
+++ src/Components/JewelryList.jsx
@@ -4,6 +4,14 @@ function JewelryList ({blings}) {
     
     return (
         <div className="jewelry__list">
+            <div className="jewelry__list__bling__header">
+                <h3>Product</h3>
+                <h3>Quantity</h3>
+                <h3>Price</h3>
+                <h3>Total value</h3>
+                <h3>In stock?</h3>
+                <h3>Last order</h3>
+            </div>
             {blings.map(bling => <Jewel key={bling.id} bling={bling}></Jewel>)}
         </div>
     )
